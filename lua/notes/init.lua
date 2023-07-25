c076714082cf4f90d145cc49487eec50854ca4c3
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-07-25 22:44:10 +0300

    feat: move to next/prev daily note with `NoteNext/Prev`

--- lua/notes/init.lua
+++ lua/notes/init.lua
@@ -12,7 +12,10 @@ end
 function M.make_commands()
     vim.cmd [[command! -nargs=1 NoteNew lua require("notes").new(<f-args>)]]
     vim.cmd [[command! NoteNow lua require("notes").now()]]
+    vim.cmd [[command! NoteNext lua require("notes").next()]]
+    vim.cmd [[command! NotePrev lua require("notes").prev()]]
 end
+
 function M.new(note_name)
     local filename = os.date("%Y-%m-%d") .. "_" .. note_name
     vim.cmd("e" .. config.note_dir .. filename .. ".md")
@@ -20,6 +23,32 @@ end
 
 function M.now()
     local filename = os.date("%Y-%m-%d")
+    vim.cmd("e" .. config.note_dir .. filename .. ".md")
+end
+
+function M.next()
+    local y, m, d = vim.fn.expand("%:t:r"):match("(%d%d%d%d)-?(%d?%d?)-?(%d?%d?)$")
+    local day = 24 * 60 * 60
+    local filename
+    if (y == nil or m == nil or d == nil) then
+        filename = os.date("%Y-%m-%d", os.time() + day)
+    else
+        local current_time = os.time({ year = y, month = m, day = d })
+        filename = os.date("%Y-%m-%d", current_time + day)
+    end
+    vim.cmd("e" .. config.note_dir .. filename .. ".md")
+end
+
+function M.prev()
+    local y, m, d = vim.fn.expand("%:t:r"):match("(%d%d%d%d)-?(%d?%d?)-?(%d?%d?)$")
+    local day = 24 * 60 * 60
+    local filename
+    if (y == nil or m == nil or d == nil) then
+        filename = os.date("%Y-%m-%d", os.time() - day)
+    else
+        local current_time = os.time({ year = y, month = m, day = d })
+        filename = os.date("%Y-%m-%d", current_time - day)
+    end
     vim.cmd("e" .. config.note_dir .. filename .. ".md")
 end
 
