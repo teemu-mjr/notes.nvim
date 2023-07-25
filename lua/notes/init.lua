04c66974117e998ad9ad692f0ab3de9de8add6d2
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-07-25 22:43:20 +0300

    feat: create named note with `NoteNew`

--- lua/notes/init.lua
+++ lua/notes/init.lua
@@ -10,7 +10,12 @@ function M.setup(user_config)
 end
 
 function M.make_commands()
+    vim.cmd [[command! -nargs=1 NoteNew lua require("notes").new(<f-args>)]]
     vim.cmd [[command! NoteNow lua require("notes").now()]]
+end
+function M.new(note_name)
+    local filename = os.date("%Y-%m-%d") .. "_" .. note_name
+    vim.cmd("e" .. config.note_dir .. filename .. ".md")
 end
 
 function M.now()
