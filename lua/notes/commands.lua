197b76fda085ba251e55007db903e7124dd77a75
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-07-29 21:25:32 +0300

    feat(commands): add note name auto complete for `Note`

--- lua/notes/commands.lua
+++ lua/notes/commands.lua
@@ -1,3 +1,4 @@
+local config = require("notes.config")
 local M = {}
 
 function M.setup()
@@ -7,6 +8,7 @@ function M.setup()
             require("notes").open(opts.fargs[1])
         end, {
             nargs = "?",
+            complete = NoteComplete
         })
     vim.api.nvim_create_user_command(
         "NoteToday", require("notes").today, {})
@@ -22,6 +24,25 @@ function M.setup()
         end, {})
     vim.api.nvim_create_user_command(
         "NoteSync", require("notes").sync, {})
+end
+
+function NoteComplete(ArgLead, CmdLine, CursorPos)
+    local pfile = io.popen(
+        "find " .. config.note_dir ..
+        " -maxdepth 1 -type f -printf '%f\n' | awk -F. '{print $1}'"
+    )
+    if pfile == nil then
+        return
+    end
+
+    local file_names = {}
+    local i = 0
+    for file in pfile:lines() do
+        i = i + 1
+        file_names[i] = file
+    end
+    pfile:close()
+    return file_names
 end
 
 return M
